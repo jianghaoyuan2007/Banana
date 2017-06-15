2510a2f39990ac9398be29544a072a8a804694cd
Stephen Chiang <jianghaoyuan2007@gmail.com>
2017-06-15 12:01:10 +0800

    Initial commit

---
+++ Sources/Banana.h
@@ -0,0 +1,20 @@
+//
+//  Banana.h
+//  Banana
+//
+//  Created by Stephen Chiang on 14/06/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Banana.
+FOUNDATION_EXPORT double BananaVersionNumber;
+
+//! Project version string for Banana.
+FOUNDATION_EXPORT const unsigned char BananaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Banana/PublicHeader.h>
+
+#import "ReplaceMe.h"
+
---
+++ Sources/ReplaceMe.m
@@ -0,0 +1,13 @@
+//
+//  ReplaceMe.m
+//  Banana
+//
+//  Created by Stephen Chiang on 14/06/2017.
+//
+//
+
+#import "ReplaceMe.h"
+
+@implementation ReplaceMe
+
+@end
